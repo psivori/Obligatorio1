212ffb974b735060f7e60ed7bb5f01ecd185bf84
Administrador <administrador@sp16.local>
2016-04-14 19:04:00 -0300

    Initial Commit

---
+++ Obligatorio1Tests/Obligatorio1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Obligatorio1Tests.swift
+//  Obligatorio1Tests
+//
+//  Created by Administrador on 14/4/16.
+//  Copyright © 2016 MICHO. All rights reserved.
+//
+
+import XCTest
+@testable import Obligatorio1
+
+class Obligatorio1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
