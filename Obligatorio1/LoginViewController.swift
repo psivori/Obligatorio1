8803170aa2ff3b78c1564d4762d8e83a851361fc
Administrador <administrador@SP16.local>
2016-04-15 22:18:23 -0300

    login pronto, trabajando en inicio

---
+++ Obligatorio1/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Obligatorio1
+//
+//  Created by Administrador on 15/4/16.
+//  Copyright © 2016 MICHO. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: UIViewController {
+
+}
